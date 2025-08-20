eb513c31bdbca73e8d8c0ab9ca3b895b5984da6d
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2025-08-20 08:16:55 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -14,7 +16,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -40,6 +41,19 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.hilt.android)
+    androidTestImplementation(libs.hilt.android.testing)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    /*implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    androidTestImplementation(libs.androidx.room.testing)*/
+
+    /*implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.datastore.core)*/
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
---
+++ app/src/androidTest/java/com/alexeyyuditsky/weatherapp/ScenarioTest.kt
@@ -0,0 +1,81 @@
+package com.alexeyyuditsky.weatherapp
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ScenarioTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun findCityAndShowWeather() = with(composeTestRule) {
+        setContent {
+            val navController: NavHostController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "findCityScreen"
+            ) {
+                composable(route = "findCityScreen") {
+                    FindCityScreen(
+                        viewModel = findCityViewModel(
+                            savedStateHandle = SavedStateHandle(),
+                            repository = FakeFindCityRepository()
+                        ),
+                        navigateToWeatherScreen = {
+                            navController.navigate("weatherScreen")
+                        }
+                    )
+                }
+
+                composable(route = "weatherScreen") {
+                    WeatherScreen(
+                        viewModel = WeatherViewModel(
+                            savedStateHandle = SavedStateHandle(),
+                            repository = FakeWeatherRepository()
+                        )
+                    )
+                }
+            }
+
+            val findCityPage = FindCityPage(composeTestRule = composeTestRule)
+            val weatherPage = WeatherPage(composeTestRule = composeTestRule)
+
+            findCityPage.input(text = "Moscow")
+            findCityPage.assertCityFound(cityName = "Moscow")
+            findCityPage.clickFoundCity(cityName = "Moscow")
+
+            weatherPage.assertCityName(cityName = "Moscow")
+            weatherPage.assertWeatherDisplayed(temp = "33°C")
+        }
+    }
+}
+
+private class FakeFindCityRepository : FindCityRepository {
+
+    override suspend fun findCity(query: String): FoundCity = if (query == "Moscow")
+        FoundCity(
+            name = "Moscow",
+            latitude = 55.7504461,
+            longitude = 37.6174943,
+        )
+    else
+        throw IllegalStateException("not supported for this test")
+
+}
+
+private class FakeWeatherRepository : WeatherRepository {
+
+
+
+}
